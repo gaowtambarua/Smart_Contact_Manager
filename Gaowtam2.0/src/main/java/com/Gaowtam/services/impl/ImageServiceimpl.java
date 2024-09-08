375a5b6763b08fe42ec1a09b3360b350eb550ad0
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-08 18:05:32 +0600

    Update ImageServiceimpl.java

--- Gaowtam2.0/src/main/java/com/Gaowtam/services/impl/ImageServiceimpl.java
+++ Gaowtam2.0/src/main/java/com/Gaowtam/services/impl/ImageServiceimpl.java
@@ -1,17 +1,59 @@
 package com.Gaowtam.services.impl;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.Gaowtam.services.ImageService;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
 
 @Service
-public class ImageServiceimpl implements ImageService{
+public class ImageServiceimpl implements ImageService {
+
+    private Cloudinary cloudinary;
+
+    // aikahen autowired na kore constrauctor add korce 2ty kora jai
+    public ImageServiceimpl(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
 
     @Override
     public String uploadImage(MultipartFile contactImage) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'uploadImage'");
+        // aikhne oi code lekha lagbe jekhane image upload hobe server er moddhe
+
+        try {
+            byte[] data = new byte[contactImage.getInputStream().available()];/* Jotto data ace toto byte er image bane jabe */
+
+            String filename=UUID.randomUUID().toString();
+            contactImage.getInputStream().read(data);
+            cloudinary.uploader().upload(data,ObjectUtils.asMap(
+                "public_id",contactImage.getOriginalFilename()
+            ));
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        } 
+
+        // return this.getUrlFromPublicId(filename);
+        return "";
     }
 
+    @Override
+    public String getUrlFromPublicId(String publicId) {
+       return cloudinary
+       .url()
+       .transformation(
+        new Transformation<>()
+        .width(500)
+        .height(500)
+        .crop("fill")
+       )
+       .generate(publicId);
+    }
+    
 }
