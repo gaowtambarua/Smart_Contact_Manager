69fe631c809f72c543d29e557a8a6ccf99fa3b34
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-09 13:47:32 +0600

    Update ImageServiceimpl.java

--- Gaowtam2.0/src/main/java/com/Gaowtam/services/impl/ImageServiceimpl.java
+++ Gaowtam2.0/src/main/java/com/Gaowtam/services/impl/ImageServiceimpl.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.Gaowtam.helpers.AppConstats;
 import com.Gaowtam.services.ImageService;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.Transformation;
@@ -24,36 +25,41 @@ public class ImageServiceimpl implements ImageService {
     @Override
     public String uploadImage(MultipartFile contactImage) {
         // aikhne oi code lekha lagbe jekhane image upload hobe server er moddhe
+        String filename = UUID.randomUUID().toString();
 
         try {
-            byte[] data = new byte[contactImage.getInputStream().available()];/* Jotto data ace toto byte er image bane jabe */
+            byte[] data = new byte[contactImage.getInputStream().available()];/*
+                                                                               * Jotto data ace toto byte er image size
+                                                                               * jabe
+                                                                               */
+            contactImage.getInputStream().read(data);///data er moddhe image er data add hoice
 
-            String filename=UUID.randomUUID().toString();
-            contactImage.getInputStream().read(data);
-            cloudinary.uploader().upload(data,ObjectUtils.asMap(
-                "public_id",contactImage.getOriginalFilename()
-            ));
+            cloudinary.uploader().upload(data, ObjectUtils.asMap(
+                    "public_id",filename));////coludinary te image er data upload hoyece mane image upload hoyece
+
+            return this.getUrlFromPublicId(filename);//coludinary thake image er size nia ashe url dicce;
 
         } catch (IOException e) {
 
             e.printStackTrace();
-        } 
+            return null;
+        }
 
-        // return this.getUrlFromPublicId(filename);
-        return "";
     }
 
     @Override
     public String getUrlFromPublicId(String publicId) {
-       return cloudinary
-       .url()
-       .transformation(
-        new Transformation<>()
-        .width(500)
-        .height(500)
-        .crop("fill")
-       )
-       .generate(publicId);
+        return cloudinary
+                .url()
+                .transformation(
+                        new Transformation<>()
+                                // .width(500)// image er width
+                                // .height(500)// image er height
+                                // .crop("fill"))
+                                .width(AppConstats.CONTACT_IMAGE_WIDTH)
+                                .height(AppConstats.CONTACT_IMAGE_HEIGHT)
+                                .crop(AppConstats.CONTACT_IMAGE_CROP))
+                .generate(publicId);
     }
-    
+
 }
