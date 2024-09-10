2dec1e4a313007aa235dcee26acd16ac4bbcbfdb
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-10 17:50:44 +0600

    Update admin.js

--- Gaowtam2.0/src/main/resources/static/js/admin.js
+++ Gaowtam2.0/src/main/resources/static/js/admin.js
@@ -1,16 +1,37 @@
 console.log("admin user");
 
-document.querySelector("#image_file_input").addEventListener('change', function(event) {
-    let file = event.target.files[0]; // Corrected: using 'files' instead of 'file'
-    if (file) { // Check if a file was selected
-        let reader = new FileReader();
+// document.querySelector("#image_file_input").addEventListener('change', function(event) {
+//     let file = event.target.files[0]; // Corrected: using 'files' instead of 'file'
+//     if (file) { // Check if a file was selected
+//         let reader = new FileReader();
 
-        reader.onload = function() {
-            document.querySelector("#upload_image_preview").setAttribute("src", reader.result);
-        };
+//         reader.onload = function() {
+//             document.querySelector("#upload_image_preview").setAttribute("src", reader.result);
+//         };
 
-        reader.readAsDataURL(file);
-    } else {
-        console.error("No file selected");
-    }
-});
+//         reader.readAsDataURL(file);
+//     } else {
+//         console.error("No file selected");
+//     }
+// });
+
+
+
+const choosePicture=document.querySelector('#image_file_input');
+
+
+choosePicture.addEventListener('change',(event)=>
+    {
+        let file = event.target.files[0]; // Corrected: using 'files' instead of 'file'
+            if (file) { // Check if a file was selected
+                let reader = new FileReader();
+        
+                reader.onload = function() {
+                    document.querySelector("#upload_image_preview").setAttribute("src", reader.result);
+                };
+        
+                reader.readAsDataURL(file);
+            } else {
+                console.error("No file selected");
+            }
+    });
