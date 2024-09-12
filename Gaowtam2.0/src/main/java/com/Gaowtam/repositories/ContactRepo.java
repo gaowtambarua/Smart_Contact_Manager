d30b6812ed50ef83888bee5e92645571eca0ec4d
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-12 17:51:40 +0600

    Update ContactRepo.java

--- Gaowtam2.0/src/main/java/com/Gaowtam/repositories/ContactRepo.java
+++ Gaowtam2.0/src/main/java/com/Gaowtam/repositories/ContactRepo.java
@@ -1,5 +1,7 @@
 package com.Gaowtam.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +18,8 @@ public interface ContactRepo extends JpaRepository<Contact,String>{
     
     //find the contact by user
     //custom finder method
-    List<Contact> findByUser(User user);
+    // List<Contact> findByUser(User user);
+    Page<Contact> findByUser(User user,Pageable pageable);
 
     //custom query method get all contacts of a user
     @Query("Select c From Contact c WHERE c.user.id= :userid")
