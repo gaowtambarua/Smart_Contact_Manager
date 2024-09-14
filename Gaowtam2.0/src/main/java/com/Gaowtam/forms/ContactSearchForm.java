5db6c202211d5bebea60cbed5b4e1b1b69d4753b
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-14 18:46:55 +0600

    Create ContactSearchForm.java

---
+++ Gaowtam2.0/src/main/java/com/Gaowtam/forms/ContactSearchForm.java
@@ -0,0 +1,11 @@
+package com.Gaowtam.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContactSearchForm {
+    private String field;
+    private String value;
+}
