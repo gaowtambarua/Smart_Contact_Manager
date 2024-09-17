907208e89fc2e427cea404341254c79c1265fd75
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-17 08:31:19 +0600

    Update script.js

--- Gaowtam2.0/src/main/resources/static/js/script.js
+++ Gaowtam2.0/src/main/resources/static/js/script.js
@@ -12,7 +12,7 @@
 // {
 //     // set to web page
 //     changePaheTheme(currentTheme,currentTheme);
-//     console.log(currentTheme);  
+//     console.log(currentTheme);
 //    //set the listener to change theme button
 //    const changeThemeButton=document.querySelector('#theme_chamge_button');
 //    changeThemeButton.addEventListener('click',(event)=>{
@@ -26,10 +26,9 @@
 //             currentTheme="dark";
 //             }
 //             changePaheTheme(currentTheme,oldTheme);
-//             console.log(currentTheme+"  "+oldTheme);  
+//             console.log(currentTheme+"  "+oldTheme);
 //    });
 // }
-
 
 // //get theme from localstorage
 // function getTheme()
@@ -59,37 +58,31 @@
 //      theme=="light"?" Dark ":" Light ";
 // }
 console.log(localStorage.getItem("theme"));
-let currentTheme=getTheme();
-const changeThemeButton=document.querySelector('#theme_chamge_button');
-changeThemeButton.querySelector('span').textContent=currentTheme=="light"?" Dark ":" Light ";
-document.querySelector('html').classList.add(currentTheme);
-document.addEventListener('DOMContentLoaded',()=>{
+let currentTheme = getTheme();
+const changeThemeButton = document.querySelector("#theme_chamge_button");
+changeThemeButton.querySelector("span").textContent =
+  currentTheme == "light" ? " Dark " : " Light ";
+document.querySelector("html").classList.add(currentTheme);
+document.addEventListener("DOMContentLoaded", () => {
   changeTheme();
 });
 
-function getTheme()
-{
-  let theme=localStorage.getItem("theme");
-  return theme?theme:"light";
+function getTheme() {
+  let theme = localStorage.getItem("theme");
+  return theme ? theme : "light";
 }
-function changeTheme()
-{
-  
-  changeThemeButton.addEventListener('click',(event)=>
-  {
-    let oldTheme=currentTheme;
-    if(currentTheme=="dark")
-    {
-      currentTheme="light";
+function changeTheme() {
+  changeThemeButton.addEventListener("click", (event) => {
+    let oldTheme = currentTheme;
+    if (currentTheme == "dark") {
+      currentTheme = "light";
+    } else {
+      currentTheme = "dark";
     }
-    else
-    {
-      currentTheme="dark";
-    }
-    document.querySelector('html').classList.remove(oldTheme);
-    document.querySelector('html').classList.add(currentTheme);
-    localStorage.setItem("theme",currentTheme);
-    changeThemeButton.querySelector('span').textContent=currentTheme=="light"?" Dark ":" Light ";
+    document.querySelector("html").classList.remove(oldTheme);
+    document.querySelector("html").classList.add(currentTheme);
+    localStorage.setItem("theme", currentTheme);
+    changeThemeButton.querySelector("span").textContent =
+      currentTheme == "light" ? " Dark " : " Light ";
   });
-  
 }
