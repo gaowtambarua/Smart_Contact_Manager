7f740a7fe79f9d5791996fb7cb58b72f061f9111
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-17 08:30:57 +0600

    Update Contact.java

--- Gaowtam2.0/src/main/java/com/Gaowtam/entities/Contact.java
+++ Gaowtam2.0/src/main/java/com/Gaowtam/entities/Contact.java
@@ -2,6 +2,8 @@ package com.Gaowtam.entities;
 
 import java.util.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -37,6 +39,7 @@ public class Contact {
     private String cloudinaryImagePublicId;
 
     @ManyToOne
+    @JsonIgnore
     private User user;///many contact thakbe ek user er jonno
 
     @OneToMany(mappedBy = "contact",cascade = CascadeType.ALL,fetch = FetchType.EAGER,orphanRemoval = true)
