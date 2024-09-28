7b6733679081acfab7d8c1de7c0149031dc88a49
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-28 21:54:16 +0600

    Update User.java

--- Gaowtam2.0/src/main/java/com/Gaowtam/entities/User.java
+++ Gaowtam2.0/src/main/java/com/Gaowtam/entities/User.java
@@ -24,8 +24,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name="user")
-@Table(name="users")
+@Entity(name = "user")
+@Table(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -34,9 +34,9 @@ import lombok.Setter;
 public class User implements UserDetails {
     @Id
     private String userId;
-    @Column(name="user_name",nullable = false)
+    @Column(name = "user_name", nullable = false)
     private String name;
-    @Column(unique = true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
     @Getter(value = AccessLevel.NONE)
     private String password;
@@ -46,40 +46,40 @@ public class User implements UserDetails {
     private String profilePic;
     private String phoneNumber;
 
-    @Getter(value=AccessLevel.NONE)//Labok er moddhe enable auto genrate hobe na
-    //information
-    private boolean enabled=true;
-    private boolean emmailVerified=false;
-    private boolean phoneVerified=false;
+    @Getter(value = AccessLevel.NONE) // Labok er moddhe enable auto genrate hobe na
+    // information
+    // private boolean enabled=true;
+    private boolean enabled = false;
+    private boolean emmailVerified = false;
+    private boolean phoneVerified = false;
 
-
-    
-    //SELF,GOOGLE,FACEBOOK,TWITER,LINKEDIN,GITHUB
-    @Enumerated(value=EnumType.STRING)
-    private Providers provider=Providers.SELF;
+    // SELF,GOOGLE,FACEBOOK,TWITER,LINKEDIN,GITHUB
+    @Enumerated(value = EnumType.STRING)
+    private Providers provider = Providers.SELF;
     private String providersUserId;
 
-    //add mode field needed
-    //Ek user er many contact thakbe
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
-    private List<Contact> contacts=new ArrayList<>();
-
-
+    // add mode field needed
+    // Ek user er many contact thakbe
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
 
     @ElementCollection(fetch = FetchType.EAGER)
-    private  List<String> roleList=new ArrayList<>();
+    private List<String> roleList = new ArrayList<>();
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // return Collections.emptyList();//Kono Role ny user er,user admin oww na,user oww na
-        
-        //portek to roleslist a ke map er maddhome alada alada korci ,tarpor SimpleGrantedAuthority er maddhome convert kore collect korci
+        // return Collections.emptyList();//Kono Role ny user er,user admin oww na,user
+        // oww na
 
-        Collection<SimpleGrantedAuthority> roles=roleList.stream().map(role-> new SimpleGrantedAuthority(role)).collect(Collectors.toList());
-        return  roles;
+        // portek to roleslist a ke map er maddhome alada alada korci ,tarpor
+        // SimpleGrantedAuthority er maddhome convert kore collect korci
+
+        Collection<SimpleGrantedAuthority> roles = roleList.stream().map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
+        return roles;
     }
 
-    //user name is email;
+    // user name is email;
     @Override
     public String getUsername() {
         return this.email;
@@ -95,6 +95,4 @@ public class User implements UserDetails {
         return this.password;
     }
 
-    
-   
 }
