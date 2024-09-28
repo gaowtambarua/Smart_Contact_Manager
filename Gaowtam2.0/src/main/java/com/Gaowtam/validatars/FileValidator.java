724a3ecf096df09af6d2dea9d616eb2dfad92c2b
Gaowtam Barua <128220812+nirob802@users.noreply.github.com>
2024-09-28 21:56:33 +0600

    Update FileValidator.java

--- Gaowtam2.0/src/main/java/com/Gaowtam/validatars/FileValidator.java
+++ Gaowtam2.0/src/main/java/com/Gaowtam/validatars/FileValidator.java
@@ -2,49 +2,47 @@ package com.Gaowtam.validatars;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.*;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class FileValidator implements ConstraintValidator<ValidFile,MultipartFile>{
+public class FileValidator implements ConstraintValidator<ValidFile, MultipartFile> {
 
-    private static final long MAX_FILE_SIZE= 1024 * 1024 * 2 ;//2 MB file size
-    //type
+    private static final long MAX_FILE_SIZE = 1024 * 1024 * 2;// 2 MB file size
+    // type
 
-    //height
+    // height
 
-    //width
+    // width
 
     @Override
     public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
-        
-        if(file==null || file.isEmpty())
-        {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("File cannot be empty").addConstraintViolation();
-            return false;
+
+        if (file == null || file.isEmpty()) {
+
+            // u
+            // context.disableDefaultConstraintViolation();
+            // context.buildConstraintViolationWithTemplate("File cannot be
+            // empty").addConstraintViolation();
+            // return false;
+            return true;
         }
-        if(file.getSize()>MAX_FILE_SIZE)
-        {
+        if (file.getSize() > MAX_FILE_SIZE) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("File size should be less than 2MB").addConstraintViolation();
             return false;
         }
-        //resolution
-        
-        // try {
-        //      BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+        // resolution
 
-        //      if(bufferedImage.getHeight()||bufferedImage.getWidth())
-        //      {
-        //         // image er hight width kmne check kora hoi dekhano hoyece
-        //      }
+        // try {
+        // BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+
+        // if(bufferedImage.getHeight()||bufferedImage.getWidth())
+        // {
+        // // image er hight width kmne check kora hoi dekhano hoyece
+        // }
         // } catch (IOException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
         // }
         return true;
     }
